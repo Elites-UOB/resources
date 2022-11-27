ec61fe0f5b128396186bad4733362413ad89da9b
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-27 22:09:43 +0300

    meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div flex="~ col gap-8" overflow-y="auto" lg:overflow-y="unset" overflow-x="hidden" lg:overflow-x="unset" >
-        
+    <div flex="~ col gap-8" overflow-y="auto" lg:overflow-y="unset" overflow-x="hidden" lg:overflow-x="unset">
+
         <div id="modals" order="2">
             <NewResource />
         </div>
 
         <!-- HEADER -->
         <Header order="1" />
- 
+
         <!-- CONTENT -->
         <div flex="~ col sm:row" gap-16 min-h="[calc(100vh-25rem)]" order="3">
             <Filter />
@@ -23,8 +23,26 @@ useHead({
     title: 'Resources - مصادر',
     bodyAttrs: {
         class: 'bg-p text-pw my-4 sm:my-12 mx-auto sm:px-6 lg:px-8 px-4 max-w-7xl',
-        dir: 'rtl',
     },
+    htmlAttrs: {
+        lang: 'ar',
+        dir: 'rtl'
+    },
+    meta: [
+        { name: 'og:title', content: 'Resources - مصادر' },
+        {
+            name: 'og:description', content: "موقع مصادر يحتوي على العديد من المصادر المفيدة مثل المواقع البرمجية والدورات التعليمية والكتب."
+        },
+        { name: 'og:image', content: "/cover.jpg" },
+        { name: 'og:url', content: route.fullPath },
+        { name: 'twitter:title', content: 'Resources - مصادر' },
+        {
+            name: 'twitter:description', content: "موقع مصادر يحتوي على العديد من المصادر المفيدة مثل المواقع البرمجية والدورات التعليمية والكتب."
+        },
+        { name: 'twitter:image', content: "/cover.jpg" },
+        { name: 'twitter:card', content: 'summary_large_image' },
+
+    ]
 })
 
 const resourceStore = useResources();
