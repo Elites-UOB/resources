510d6f97f0e7900d2bbd7cf1d9529c592462b512
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-27 22:39:48 +0300

    Update Button.vue

--- components/Ui/Button.vue
+++ components/Ui/Button.vue
@@ -1,6 +1,10 @@
 <template>
-    <button :alt="alt" :class="{ 'w-14 h-14': square }" flex justify-center items-center gap-2 transition="all duration-200" rounded="15px" text="lg center pw hover:white" font-bold border="1 s-stroke" p="3" bg="s hover:s-hover" cursor="pointer">
+    <button class="group" relative="~" :class="{ 'w-14 h-14': square }" flex justify-center items-center gap-2 transition="all duration-200" rounded="15px" text="lg center pw hover:white" font-bold border="1 s-stroke" p="3" bg="s hover:s-hover" cursor="pointer">
         <slot />
+
+        <div v-if="alt" group-hover:flex hidden absolute z="30" px-3 py-2 bottom="-45px" w="full" h="20px" items-center justify-center text="10px" bg="dark" rounded-10px transition="all duration-200">
+            {{alt}}
+        </div>
     </button>
 </template>
 
