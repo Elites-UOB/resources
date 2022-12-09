fffa9d7914a30e8b7dca589e93f5debe7eebf89a
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-12-09 15:20:11 +0300

    colors

--- components/Ui/Input.vue
+++ components/Ui/Input.vue
@@ -1,9 +1,9 @@
 <template>
     <div relative flex="~" v-if="sm" >
-        <input :type="type" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="placeholder" bg="p dark:pd focus:s dark:focus:sdd" p="x-2 sm:x-4" focus:outline-none text="pw dark:pw focus:black dark:focus:white sm" font-medium border="2 s-stroke dark:sdd-stroke focus:pw dark:focus:pwd rounded-10px" />
+        <input :type="type" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="placeholder" bg="s dark:sdd focus:s dark:focus:sdd" p="x-2 sm:x-4" focus:outline-none text="pw dark:pwd focus:black dark:focus:white sm" font-medium border="2 s-stroke dark:sdd-stroke focus:pw dark:focus:pwd rounded-10px" />
     </div>
     <div relative flex="~" v-else>
-        <input :type="type" w="full" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="placeholder" bg="p dark:pd focus:s dark:focus:sdd" pr="12" pl="4" focus:outline-none text="pw dark:pw focus:black dark:focus:white lg" font-medium border="2 s-stroke dark:sdd-stroke focus:pw dark:focus:pwd rounded-15px" h="50px" />
+        <input :type="type" w="full" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="placeholder" bg="s dark:sdd focus:s dark:focus:sdd" pr="12" pl="4" focus:outline-none text="pw dark:pwd focus:black dark:focus:white lg" font-medium border="2 s-stroke dark:sdd-stroke focus:pw dark:focus:pwd rounded-15px" h="50px" />
         <icon v-if="icon" absolute :name="icon" w="24px" h="24px" top="16px" right="4" />
     </div>
 </template>
