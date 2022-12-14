1cc87f643e896f716002745849f8dc60cc2c20f6
william-iq397 <93004634+william-iq397@users.noreply.github.com>
2022-12-14 20:55:07 +0300

    Banner has been added

--- components/Resources.vue
+++ components/Resources.vue
@@ -1,18 +1,23 @@
 <template>
-    <div w="full" flex="~ col gap-3"  v-if="resourcesStore.getFilteredResources?.length > 0">
-        <Resource v-for="resource in resourcesStore.getFilteredResources" :resource="resource" :key="resource.id" />
-        <Skeleton v-show="loading" />
-    </div>
-    <div v-else text="pw dark:pwd lg" flex="~ col gap-4" items-center justify-center bg="p dark:pd" w="auto sm:full"
-        h="200px" font-medium border="2 s-stroke dark:sdd-stroke rounded-15px" p="6">
-        <icon name="fluent-emoji-flat:face-with-head-bandage" saturate-0 brightness-95 dark:brightness-40 w="16"
-            h="16" />
-        <span text-4xl text-d>لايوجد مصادر</span>
+    <div class="flex flex-col gap-2 h-full w-full" >
+        <section h="auto" w="full">
+            <img :src="ResourcesBanner" rounded="lg" alt="Resources Banner" w="full" h="100%"/>
+        </section>
+        <section w="full" flex="~ col gap-3"  v-if="resourcesStore.getFilteredResources?.length > 0">
+            <Resource v-for="resource in resourcesStore.getFilteredResources" :resource="resource" :key="resource.id" />
+            <ResourcesSkeleton/>
+        </section>
+        <section v-else text="pw dark:pwd lg" flex="~ col gap-4" items-center justify-center bg="p dark:pd" w="auto sm:full"
+            h="200px" font-medium border="2 s-stroke dark:sdd-stroke rounded-15px" p="6">
+            <icon name="fluent-emoji-flat:face-with-head-bandage" saturate-0 brightness-95 dark:brightness-40 w="16"
+                h="16" />
+            <span text-4xl text-d>لايوجد مصادر</span>
+        </section>
     </div>
 </template>
 
 <script setup>
-
+import ResourcesBanner from '../public/ResourcesBanner.png'
 const resourcesStore = useResources()
 const fetchMore = resourcesStore.fetchMore
 const loading = ref(false)
--- components/ResourcesSkeleton.vue
+++ components/ResourcesSkeleton.vue
@@ -1,7 +1,7 @@
 <template>
     <div w-full>
-        <div class="animate-pulse flex gap-6 p-4" border="~ rounded-15px s-stroke dark:sdd-stroke 3px rounded-15px">
-            <section class="rounded-md bg-gray-400 dark:bg-sdd-hover h-8 w-8"></section>
+        <div class="animate-pulse flex gap-6 p-4" w="full" border="~ rounded-15px s-stroke dark:sdd-stroke 3px rounded-15px">
+            <div class="rounded-md bg-gray-400 dark:bg-sdd-hover h-25px w-25px"></div>
             <div class="flex-1 space-y-3 py-1">
                 <div flex gap-4>
                      <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded col-span-1 w-50"/>
@@ -11,8 +11,7 @@
                     <div class="grid grid-cols-3 gap-4"/>
                 </div>
             </div>
-            <section class="rounded-full bg-gray-400 dark:bg-sdd-hover h-10 w-10"></section>
-
+            <div class="rounded-full bg-gray-400 dark:bg-sdd-hover h-25px w-25px"></div>
         </div>
     </div>
 </template>
