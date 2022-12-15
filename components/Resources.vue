ae5085d8443b128e05aad092a80ea6db0b8cea4f
william-iq397 <93004634+william-iq397@users.noreply.github.com>
2022-12-15 03:25:59 +0300

    banner and UI issues solved

--- components/Resources.vue
+++ components/Resources.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="flex flex-col gap-2 h-full w-full" >
-        <section h="auto" w="full">
-            <img :src="ResourcesBanner" rounded="lg" alt="Resources Banner" w="full" h="100%"/>
+    <div class="flex flex-col gap-2 h-full w-full justify-center items-center">
+        <section v-if="!resourcesStore.filters.search" @click="displayMethodologies()" select="none" ref="BannerContainer" h="auto" w="full" cursor="pointer" title="عرض المناهج" >
+            <img :src="ResourcesBanner" border="rounded-15px" alt="Resources Banner" w="full" h="100%"/>
         </section>
         <section w="full" flex="~ col gap-3"  v-if="resourcesStore.getFilteredResources?.length > 0">
             <Resource v-for="resource in resourcesStore.getFilteredResources" :resource="resource" :key="resource.id" />
-            <ResourcesSkeleton/>
+            <ResourcesSkeleton v-show="loading" />
         </section>
-        <section v-else text="pw dark:pwd lg" flex="~ col gap-4" items-center justify-center bg="p dark:pd" w="auto sm:full"
-            h="200px" font-medium border="2 s-stroke dark:sdd-stroke rounded-15px" p="6">
-            <icon name="fluent-emoji-flat:face-with-head-bandage" saturate-0 brightness-95 dark:brightness-40 w="16"
-                h="16" />
+        <section v-else text="pw dark:pwd lg" flex="~ col gap-4" items-center justify-center bg="p dark:pd" w="full"
+            h="200px" font-medium border="2 s-stroke dark:sdd-stroke rounded-15px" py-6>
+            <icon name="fluent-emoji-flat:face-with-head-bandage" saturate-0 brightness-95 dark:brightness-40 w="16" h="16" />
             <span text-4xl text-d>لايوجد مصادر</span>
         </section>
     </div>
@@ -21,13 +20,16 @@ import ResourcesBanner from '../public/ResourcesBanner.png'
 const resourcesStore = useResources()
 const fetchMore = resourcesStore.fetchMore
 const loading = ref(false)
+const BannerContainer = ref(null)
+
+function displayMethodologies() {
+    resourcesStore.filters.search = 'منهج المرحلة'
+}
 
 useInfiniteScroll(document, async () => {
     loading.value = true
     await fetchMore()
-    setTimeout(() => {
-        loading.value = false
-    }, 2000);
+    loading.value = false
 }, { distance: 15 })
 
 </script>
--- components/ResourcesSkeleton.vue
+++ components/ResourcesSkeleton.vue
@@ -1,17 +1,21 @@
 <template>
     <div w-full>
-        <div class="animate-pulse flex gap-6 p-4" w="full" border="~ rounded-15px s-stroke dark:sdd-stroke 3px rounded-15px">
-            <div class="rounded-md bg-gray-400 dark:bg-sdd-hover h-25px w-25px"></div>
-            <div class="flex-1 space-y-3 py-1">
-                <div flex gap-4>
+        <div class="animate-pulse flex gap-1 py-4 px-4 items-center justify-between" w="full" border="~ rounded-15px s-stroke dark:sdd-stroke 3px rounded-15px">
+            <div class="rounded-md bg-gray-400 dark:bg-sdd-hover" w="20px md:30px" h="20px md:30px"/>
+            <div class="flex-1 space-y-3 px-4">
+                <div flex gap="3" w="40%">
                      <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded col-span-1 w-50"/>
-                     <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded col-span-1 w-30"/>
+                     <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded col-span-1 w-30 "/>
                  </div>
-                <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded">
-                    <div class="grid grid-cols-3 gap-4"/>
-                </div>
+                <div class="h-2 bg-gray-400 dark:bg-sdd-hover rounded" w="60 sm:full"/>
             </div>
-            <div class="rounded-full bg-gray-400 dark:bg-sdd-hover h-25px w-25px"></div>
+            <div class="rounded-full bg-gray-400 dark:bg-sdd-hover" w="20px md:30px" h="20px md:30px"/>
         </div>
     </div>
 </template>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
